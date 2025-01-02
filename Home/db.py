b5e8f2472f281a005715e7bd78db1e4dd84c8a22
Htijaro <139818497+Htijaro@users.noreply.github.com>
2025-01-01 23:13:53 -0500

    Update db.py

--- Home/db.py
+++ Home/db.py
@@ -6,7 +6,7 @@ MYSQL = {
         'NAME': 'dbh2o',
         'USER': os.getenv('DATABASE_USER', 'myuser'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'ubuntu08-'),
-        'HOST': os.getenv('DATABASE_HOST', 'db'),
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
         'PORT': os.getenv('DATABASE_PORT','3306'),
                                 'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
